28d20e49a78fd9e6352798a69958109736289a80
Vilas Jadhav <vilasj@coreflexsolutions.com>
2019-11-05 23:20:51 -0800

    check Auto Changes Ionic Pro in Master

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,10 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Pro } from '@ionic/pro';
 
 @NgModule({
-  declarations: [AppComponent],
+   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
@@ -20,4 +21,6 @@ import { AppComponent } from './app.component';
   ],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule {}
