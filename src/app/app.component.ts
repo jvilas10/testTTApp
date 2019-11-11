b7c2fc04748348c347aa328aeedffbc23217cbd0
Vilas Jadhav <vilasj@coreflexsolutions.com>
2019-11-11 03:12:48 -0800

    Produnction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Pro, AppInfo, DeployInfo } from '@ionic-native/pro';
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private pro: Pro
   ) {
     this.initializeApp();
   }
@@ -24,4 +26,15 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  // Get app info
+this.pro.getAppInfo().then((res: AppInfo) => {
+  console.log(res)
+})
+
+// Get live update info
+this.pro.deploy.info().then((res: DeployInfo) => {
+  console.log(res)
+})
+
 }
