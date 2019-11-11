e2a80a17a50d5d8526faf0312375cdcbad629dd6
Vilas Jadhav <vilasj@coreflexsolutions.com>
2019-11-11 03:09:31 -0800

    Produnction

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -12,16 +12,6 @@ import { Pro, AppInfo, DeployInfo } from '@ionic-native/pro/ngx';
 })
 export class Tab1Page {
 
-  constructor(private pro: Pro) { }
 
-  // Get app info
-this.pro.getAppInfo().then((res: AppInfo) => {
-  console.log(res)
-})
-
-// Get live update info
-this.pro.deploy.info().then((res: DeployInfo) => {
-  console.log(res)
-})
 
 }
