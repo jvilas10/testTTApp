ec0303c297a611d7008bd7c6c49937878940138e
Vilas Jadhav <vilasj@coreflexsolutions.com>
2019-11-11 03:36:39 -0800

    Chnage Produ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Pro, AppInfo, DeployInfo } from '@ionic-native/pro';
+//import { Pro, AppInfo, DeployInfo } from '@ionic-native/pro';
+import { Pro} from '@ionic/pro';
+import {AppInfo,Deploy } from 'cordova-plugin-ionic';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +17,8 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private pro: Pro
+    private pro: Pro,
+    private deploy: Deploy
   ) {
     this.initializeApp();
   }
@@ -27,19 +30,21 @@ export class AppComponent {
     });
   }
 
-  // Get app info
- pro.getAppInfo().then((res: AppInfo) => {
-  console.log(res)
+//   // Get app info
+//  pro.getAppInfo().then((res: AppInfo) => {
+//   console.log(res)
 
-})
+// })
 
-// Get live update info
- pro.deploy.info().then((res: DeployInfo) => {
-  console.log(res)
-})
+// // Get live update info
+//  pro.deploy.info().then((res: DeployInfo) => {
+//   console.log(res)
+// })
 
-  async changeToBetaChannel() {  
+async changeToBetaChannel() {
+  alert('production');
   await Deploy.configure({channel: 'production'});
+  //Deploy.info().then
 }
 
 }
