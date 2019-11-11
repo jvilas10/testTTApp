7acbbfbffd5db39d489dbc9c7678c5e0b413bb3d
Vilas Jadhav <vilasj@coreflexsolutions.com>
2019-11-11 05:08:03 -0800

    Create code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-//import { Pro, AppInfo, DeployInfo } from '@ionic-native/pro';
-import { Pro} from '@ionic/pro';
-import { Deploy } from 'cordova-plugin-ionic';
+
 
 @Component({
   selector: 'app-root',
@@ -17,8 +15,6 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private pro: Pro,
-  
   ) {
     this.initializeApp();
   }
@@ -30,21 +26,5 @@ export class AppComponent {
     });
   }
 
-//   // Get app info
-//  pro.getAppInfo().then((res: AppInfo) => {
-//   console.log(res)
-
-// })
-
-// // Get live update info
-//  pro.deploy.info().then((res: DeployInfo) => {
-//   console.log(res)
-// })
-
-async changeToBetaChannel() {
-  alert('production');
-  await Deploy.configure({channel: 'production'});
-  //Deploy.info().then
-}
 
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,10 +1,81 @@
+// settings.ts
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { Pro } from '@ionic/pro';
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: 'page-settings',
+  templateUrl: 'settings.html'
 })
-export class Tab1Page {
-  constructor() { }
-}
+
+  export class Tab1Page {
+
+    public deployChannel = "";
+    public isBeta = false;
+    public downloadProgress = 0;
+  
+    constructor(public navCtrl: NavController) {
+      this.checkChannel();
+    }
+  
+    async checkChannel() {
+      try {
+        const res = await Pro.deploy.getConfiguration();
+        this.deployChannel = res.channel;
+        this.isBeta = (this.deployChannel === 'Beta')
+      } catch (err) {
+        // We encountered an error.
+        // Here's how we would log it to Ionic Pro Monitoring while also catching:
+  
+        // Pro.monitoring.exception(err);
+      }
+    }
+  
+    async toggleBeta() {
+      const config = {
+        channel: (this.isBeta ? 'Master' : 'Production')
+      }
+  
+      try {
+        await Pro.deploy.configure(config);
+        await this.checkChannel();
+        await this.sync({updateMethod: 'auto'}); // Alternatively, to customize how this works, use performManualUpdate()
+      } catch (err) {
+        // We encountered an error.
+        // Here's how we would log it to Ionic Pro Monitoring while also catching:
+  
+        // Pro.monitoring.exception(err);
+      }
+  
+    }
+  
+    async performManualUpdate() {
+  
+      /*
+        Here we are going through each manual step of the update process:
+        Check, Download, Extract, and Redirect.
+  
+        Ex: Check, Download, Extract when a user logs into your app,
+          but Redirect when they logout for an app that is always running
+          but used with multiple users (like at a doctors office).
+      */
+  
+      try {
+        const update = await Pro.deploy.checkForUpdate();
+  
+        if (update.available){
+          this.downloadProgress = 0;
+  
+          await Pro.deploy.downloadUpdate((progress) => {
+            this.downloadProgress = progress;
+          })
+          await Pro.deploy.extractUpdate();
+          await Pro.deploy.reloadApp();
+        }
+      } catch (err) {
+        // We encountered an error.
+        // Here's how we would log it to Ionic Pro Monitoring while also catching:
+      // Pro.monitoring.exception(err);
+      } }
+  }
